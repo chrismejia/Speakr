1172fa4f5f944ad2220576f400ff743660098595
Christian Mejia <chrismejia10@gmail.com>
2018-11-12 00:00:14 -0500

    Add Google log in page

---
+++ components/LoggedInPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const LoggedInPage = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome:{props.name}</Text>
+      <Image style={styles.image} source={{ uri: props.photoUrl }} />
+    </View>
+  );
+};
